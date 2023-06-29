df1de63c233c08badfaf3469b1b3a1b4bd26744f
sekine-kibo <Kibo.S.Hope@gmail.com>
2023-06-29 23:52:03 +0900

    init

---
+++ migrate/migrate.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"german-spitz/db"
+	"german-spitz/model"
+)
+
+func main() {
+	dbConn := db.NewDB()
+	defer fmt.Println("Successfully Migrated")
+	defer db.CloseDB(dbConn)
+	dbConn.AutoMigrate(&model.User{}, &model.Task{})
+}
